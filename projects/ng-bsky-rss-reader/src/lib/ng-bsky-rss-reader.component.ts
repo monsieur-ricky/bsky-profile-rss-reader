3512c820c0ebe142c32266fb068271050c41b9d3
Ricky <ricky2k7@gmail.com>
2025-01-01 00:34:17 +0000

    feat: Create ng-bsky-rss-reader

---
+++ projects/ng-bsky-rss-reader/src/lib/ng-bsky-rss-reader.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ng-bsky-rss-reader',
+  imports: [],
+  template: `
+    <p>
+      ng-bsky-rss-reader works!
+    </p>
+  `,
+  styles: ``
+})
+export class NgBskyRssReaderComponent {
+
+}
---
+++ projects/ng-bsky-rss-reader/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ng-bsky-rss-reader
+ */
+
+export * from './lib/ng-bsky-rss-reader.service';
+export * from './lib/ng-bsky-rss-reader.component';
