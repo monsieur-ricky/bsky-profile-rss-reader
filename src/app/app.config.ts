09223c0e479d6e5d5ac81c91dd1ede29a33bc425
Ricky <ricky2k7@gmail.com>
2025-01-02 17:00:49 +0000

    improvement: Remove RxJS and Router dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import {
+  ApplicationConfig,
+  provideExperimentalZonelessChangeDetection
+} from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes)]
+  providers: [provideExperimentalZonelessChangeDetection()]
 };
