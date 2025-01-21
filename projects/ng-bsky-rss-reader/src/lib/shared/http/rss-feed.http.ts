62442012c7f4dcbefcdf2a5ca1795ed7e12828bb
Ricky <ricky2k7@gmail.com>
2025-01-21 18:48:27 +0100

    feat: Replace CORS Proxy
    Replace allOrigins CORS Proxy with custom solution.

--- projects/ng-bsky-rss-reader/src/lib/shared/http/rss-feed.http.ts
+++ projects/ng-bsky-rss-reader/src/lib/shared/http/rss-feed.http.ts
@@ -3,8 +3,9 @@ import { FeedData, Profile, RssFeed } from '../models/rss-feed.model';
 
 @Injectable({ providedIn: 'root' })
 export class RssFeedHttpService {
-  // To avoid CORS errors, https://allorigins.win/ is being used as a proxy to request the RSS feed
-  private readonly allowOrigins = 'https://api.allorigins.win/raw?url=';
+  // To avoid CORS errors, a proxy is being used to request the RSS feed
+  private readonly allowOrigins =
+    'https://cors.monsieur-ricky.workers.dev/?url=';
   private readonly bskyRssUrl = 'https://bsky.app/profile/{id}/rss';
 
   loadFeed(profile: string): Promise<FeedData> {
